b4902d4bf4fb2521ccd47504ea11550b144f65e5
IakoGM <jakgonza@student.42urduliz.com>
2023-07-06 10:37:26 +0200

    buffer allocated in heap - working fine

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jakgonza <jakgonza@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 12:16:34 by jakgonza          #+#    #+#             */
-/*   Updated: 2023/07/04 11:54:21 by jakgonza         ###   ########.fr       */
+/*   Updated: 2023/07/06 10:34:48 by jakgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,20 @@
 
 char	*ft_read(int fd, char *store)
 {
-	char	buf[BUFFER_SIZE + 1];
+	char	*buf;
 	ssize_t	bits_read;
 
 	bits_read = 1;
-	if (!store)
-	{
-		store = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-		store[0] = '\0';
-	}
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
 	while (bits_read > 0)
 	{
 		bits_read = read(fd, buf, BUFFER_SIZE);
 		if (bits_read == -1)
 		{
 			free(store);
+			free(buf);
 			return (NULL);
 		}
 		buf[bits_read] = '\0';
@@ -36,6 +35,7 @@ char	*ft_read(int fd, char *store)
 		if (ft_find(store, '\n') != 0)
 			break ;
 	}
+	free(buf);
 	return (store);
 }
 
@@ -46,6 +46,11 @@ char	*get_next_line(int fd)
 
 	if (BUFFER_SIZE <= 0 || fd == -1)
 		return (NULL);
+	if (!store)
+	{
+		store = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		store[0] = '\0';
+	}
 	store = ft_read(fd, store);
 	if (!store)
 		return (NULL);
